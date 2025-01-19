35f78c68aabb3d174c40766047b9bc4e14cb9921
l15y <ly444983@live.com>
2025-01-20 00:03:57 +0800

    remove MishGLU

--- model.py
+++ model.py
@@ -247,36 +247,6 @@ class RWKV_CMix_x070(MyModule):
 
         return self.value(k)
 
-########################################################################################################
-
-class MishGLU(MyModule):
-    def __init__(self, args, layer_id):
-        super().__init__()
-        self.args = args
-        self.layer_id = layer_id
-        self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
-
-        with torch.no_grad():
-            ratio_1_to_almost0 = 1.0 - (layer_id / args.n_layer)
-
-            x = torch.ones(1, 1, args.n_embd)
-            for i in range(args.n_embd):
-                x[0, 0, i] = i / args.n_embd
-
-            self.time_mix_k = nn.Parameter(torch.pow(x, ratio_1_to_almost0))
-            self.time_mix_r = nn.Parameter(torch.pow(x, ratio_1_to_almost0))
-            self.aa = nn.Linear(args.n_embd, args.dim_ffn, bias=False)
-            self.bb = nn.Linear(args.n_embd, args.dim_ffn, bias=False)
-            self.value = nn.Linear(args.dim_ffn, args.n_embd, bias=False)
-
-    @MyFunction
-    def forward(self, x):
-        xx = self.time_shift(x)
-        xa = x * self.time_mix_k + xx * (1 - self.time_mix_k)
-        xb = x * self.time_mix_r + xx * (1 - self.time_mix_r)
-        a = self.aa(xa)
-        b = self.bb(xb)
-        return self.value(a * F.mish(b))
 
 ########################################################################################################
 # The RWKV Model with our blocks
